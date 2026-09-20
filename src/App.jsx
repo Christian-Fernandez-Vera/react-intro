1db8cd68c4a20100233f049f578a85bea7a855c6
Christian Fernandez Vera <xtian.fdz@hotmail.com>
2026-09-20 19:51:38 +0200

    eliminazione css e add header, main, footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 function App() {
   // rendering in UI
-  return <h1>First react app</h1>;
+  return <div>
+      <header>
+        30 linee per header
+      </header>
+
+      <main>
+        <section>
+          code section
+        </section>
+      </main>
+
+      <footer>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur minima dolores magni ratione doloribus, praesentium nemo labore a ullam aliquam veritatis vero voluptas fuga officiis reprehenderit aliquid delectus nesciunt necessitatibus?
+      </footer>
+
+
+  </div>
+  
 }
 
 export default App;
